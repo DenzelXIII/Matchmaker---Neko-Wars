673446e65f6c5e9d27b3e1954e6f4285e7a70145
DenzelXIII <denzel_justin@hotmail.com>
2016-04-26 12:27:24 +0200

    first commit

---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public Grid grid;
+
+    private int level = 3;
+
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        grid.SetUpScene(level);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_scripts/TileSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSystem : MonoBehaviour
+{
+    public GameObject tile;
+    public Vector2 mapSize;
+
+    [Range(0,1)]
+    public float outline;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        TileSpawner();
+	}
+
+    public void TileSpawner()
+    {
+        string holderName = "map";
+        Transform mapHolder = new GameObject(holderName).transform;
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePos = new Vector3(-mapSize.x / 2 + x, 0, -mapSize.y / 2 + y);
+                Transform newTile = Instantiate(tile, tilePos, Quaternion.Euler(Vector3.right*90)) as Transform;
+               // newTile.parent = mapHolder.transform;
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
